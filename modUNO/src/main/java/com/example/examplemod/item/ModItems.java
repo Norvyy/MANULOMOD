90386d729f92759b83f0cc6ee2f8cf13f490624c
Norvyy <detakira67@gmail.com>
2024-08-07 23:01:06 +0200

    aaa
    aaa

--- modUNO/src/main/java/com/example/examplemod/item/ModItems.java
+++ modUNO/src/main/java/com/example/examplemod/item/ModItems.java
@@ -1,5 +1,16 @@
 package com.example.examplemod.item;
 
+import com.example.examplemod.ManuloMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
 public class ModItems {
-    
+    private static final DeferredRegister<Item> ITEMS= DeferredRegister.create(ITEMS,ManuloMod.MODID);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
